b3a6274dbf591437e9a30d195382674ed9bc83a9
Luke Miller <lam59@buffalo.edu>
2023-01-31 14:21:49 -0500

    Added Dockerfile and changed climbs to production URL.

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -2,7 +2,9 @@ import Link from "next/link";
 import Image from 'next/image';
 
 async function getClimbs() {
-    const res = await fetch('http://127.0.0.1:8090/api/collections/boulders/records?page=1&perPage=30');
+    // const res = await fetch('http://127.0.0.1:8090/api/collections/boulders/records?page=1&perPage=30');
+    const res = await fetch('--http=0.0.0.0:8080/api/collections/boulders/records?page=1&perPage=30');
+
     const data = await res.json();
     return data?.items as any[];
 }
