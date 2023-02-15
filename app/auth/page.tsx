ac36841edab57effc3814bcdeafd125b0d69f5ee
Luke Miller <lam59@buffalo.edu>
2023-02-15 09:33:03 -0800

    Adding dynamic export: https://github.com/vercel/next.js/issues/43077

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+export const dynamic='force-dynamic';
+
 import { redirect } from 'next/navigation';
 
 // Import pocketbase
@@ -63,9 +65,9 @@ export default async function Auth({searchParams,}: {searchParams?: { [key: stri
             </defs>
           </svg>
       </div>
-      { searchParams.mode === 'verifyEmail' ? <VerifyEmail result={result}/> : null}
-      { searchParams.mode === 'updateEmail' ? <UpdateEmail token={searchParams.token}/> : null}
-      { searchParams.mode === 'resetPassword' ? <ResetPassword token={searchParams.token}/> : null}
+      { searchParams['mode'] === 'verifyEmail' ? <VerifyEmail result={result}/> : null}
+      { searchParams['mode'] === 'updateEmail' ? <UpdateEmail token={searchParams.token}/> : null}
+      { searchParams['mode'] === 'resetPassword' ? <ResetPassword token={searchParams.token}/> : null}
       <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
             <svg
               className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
